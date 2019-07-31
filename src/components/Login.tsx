97c321be88bae3f3c1ce27d12fbe7daab3fe020c
deanfchung <deanthechung@gmail.com>
2019-07-30 22:34:01 -0700

    axios request signup form, changed server port to 3000

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { Nav } from '../utils';
-import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 
 const Login: React.FC = (props: any): JSX.Element => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onUserChange = (e) => {
+    console.log(e.target.value)
+    setUsername(e.target.value);
+  }
+
+  const onPasswordChange = (e) => {
+    console.log(e.target.value);
+    setPassword(e.target.value);
+  }
   return (
+   
     <div id="Login">
-    <input type = 'text'></input>
-    <input type = 'password'></input>
+    <input onChange = { onUserChange } type = 'text'></input>
+    <input onChange = { onPasswordChange } type = 'password'></input>
     <Link to = '/'><button>Sign In</button></Link>
+    <Link to = '/signup'><button>Sign Up</button></Link>
   </div>
+
   );
 };
 
