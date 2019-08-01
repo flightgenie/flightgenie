b174d7bbe08b1636ec5a106d2869ebca8eada178
Jacob Richards <jacob.richards33@gmail.com>
2019-08-01 13:30:48 -0700

    changed browserrouter to hash and made a build

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,53 @@
-const status = process.env.NODE_ENV;
-  const path = require('path');
-  
-  module.exports = {
+const mode = process.env.NODE_ENV;
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
     entry: './src/index.tsx',
     output: {
-      path: path.resolve(__dirname, 'build'),
-      filename: 'bundle.js'
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
     },
     devtool: 'source-map',
     resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.json']
+        extensions: ['.ts', '.tsx', '.js', '.json'],
     },
-    mode: status,
+    mode: mode,
     devServer: {
-      publicPath: '/build/'
+        publicPath: '/build/',
     },
     module: {
-      rules: [
-        { test: /.tsx?$/, exclude: /node-modules/, loader: 'babel-loader' },
-        {
-          enforce: 'pre',
-          test: /.js$/,
-          exclude: /node-modules/,
-          loader: 'source-map-loader'
-        },
-        {
-          test: /.css$/,
-          use: ['style-loader', 'css-loader', 'sass-loader']
-        }
-      ]
-    }
-  };
+        rules: [
+            { test: /\.(ts|tsx)$/, exclude: /node-modules/, loader: 'babel-loader' },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                enforce: 'pre',
+                test: /.js$/,
+                exclude: /node-modules/,
+                loader: 'source-map-loader',
+            },
+            {
+                test: /.css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './index.html',
+            filename: './index.html',
+        }),
+    ],
+};
