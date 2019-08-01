5b55feb7801872233b51d0879a34428b1381266a
Jacob Richards <jacob.richards33@gmail.com>
2019-08-01 16:06:12 -0700

    chg slash routes

--- src/actions/appActions.ts
+++ src/actions/appActions.ts
@@ -10,7 +10,6 @@ import {
 } from './appActionTypes';
 import { destination, form, layoverLocation, trip } from '../Interfaces';
 
-
 export const submitForm = (form: form) => {
     console.log('this is what imt gettoimg;', form);
     const formCopy = Object.assign({}, form);
@@ -50,7 +49,7 @@ export const submitForm = (form: form) => {
     delete formCopy.endDate;
 
     return async (dispatch: Dispatch) => {
-        axios.post('http://localhost:3000/search', formCopy).then(({ data }) => {
+        axios.post('/search', formCopy).then(({ data }) => {
             console.log(data);
             dispatch<submitFormActionInterface>({
                 type: appActionTypes.SUBMIT_FORM,
@@ -63,7 +62,7 @@ export const submitForm = (form: form) => {
 export const getTrips = (username: string) => {
     return async (dispatch: Dispatch) => {
         // your code here ! add your own payload to the dispatched action.
- 
+
         dispatch<getTripsActionInterface>({
             type: appActionTypes.GET_TRIPS,
         });
@@ -74,13 +73,13 @@ export const addTrip = (tripIndex: trip) => {
     tripCopy.userId = '578df3efb618f5141202a196';
     return async (dispatch: Dispatch) => {
         // your code here ! add your own payload to the dispatched action.
-        axios.post('http://localhost:3000/trips', tripCopy).then(({data}) => {
+        axios.post('/trips', tripCopy).then(({ data }) => {
             console.log('after axios addtrip', data);
             dispatch<addTripActionInterface>({
                 type: appActionTypes.ADD_TRIP,
-                payload: tripIndex
+                payload: tripIndex,
             });
-        })
+        });
     };
 };
 export const focusTrip = (tripIndex: number): focusTripActionInterface => {
